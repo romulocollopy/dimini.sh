45e2808b6b4772c246621475b88bd7498e3eb516
Rômulo Collopy <romulo.collopyl@freshbooks.com>
2020-10-13 12:15:17 +0200

    add frontend

---
+++ public/js/main.js
@@ -0,0 +1,64 @@
+(function(){
+  const ERROR_MESSAGE = "Sory, something whent wrong... \n\n"
+
+  class Page{
+    constructor(body){
+      this.form = body.querySelector("form")
+      this.result_box = body.querySelector("div.result")
+      this.result_text = this.result_box.querySelector("#shortened")
+      this.error_text = this.result_box.querySelector("#error")
+      this.init_form()
+    }
+
+    init_form(){
+      this.form.addEventListener("submit", this.submit.bind(this))
+    }
+
+    submit(evt) {
+      evt.preventDefault()
+      this.post()
+    }
+
+    post() {
+      const method = this.form.getAttribute('method'),
+        url = this.form.getAttribute('action');
+
+      var oReq = new XMLHttpRequest();
+      oReq.addEventListener("load", this.onSuccess());
+      oReq.addEventListener("error", this.onError.bind(this));
+
+      oReq.open(method, url);
+      oReq.setRequestHeader("Accept", "application/json")
+      oReq.send();
+    }
+
+    onSuccess(a, b, c) {
+      const _this = this;
+
+      return function() {
+        if (this.status > 400) {
+          return _this.onError().bind(this)()
+        }
+
+        try {
+          response = JSON.parse(this.responseText).short_code
+        } catch {
+          response = this.responseText
+        }
+        _this.error_text.innerHTML = ''
+        _this.result_text.innerHTML = response
+      }
+    }
+
+    onError(resp) {
+      const _this = this;
+      return function() {
+        resp = this.responseText || resp;
+        _this.error_text.innerHTML = `${ERROR_MESSAGE} ${resp}`;
+      }
+    }
+
+  }
+  const body = document.querySelector('body')
+  const page = new Page(body)
+})()
